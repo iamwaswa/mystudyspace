86b651c27f27f9e1723a6713be5bf6afea8f4641
waswaolunga <olungaw@gmail.com>
2019-03-20 11:22:56 -0700

    Use  javascript to configure grid

---
+++ src/public/scripts/studyspaces.js
@@ -0,0 +1,35 @@
+const listContainer = document.querySelector(`ul`);
+const listItems = document.querySelectorAll(`li`);
+
+window.addEventListener(`load`, () => {
+  configureGrid();
+});
+
+window.addEventListener(`resize`, () => {
+  configureGrid();
+});
+
+const configureGrid = () => {
+  if (window.innerWidth >= 0) {
+    listContainer.style.gridTemplateRows = `repeat(${listItems.length}, auto)`;
+    listContainer.style.gridRowGap = `2rem`;
+    listContainer.style.gridTemplateColumns = `100%`;
+  }
+  if (window.innerWidth >= 460) {
+    listContainer.style.gridTemplateRows = `repeat(${Math.round(listItems.length / 2)}, auto)`;
+    listContainer.style.gridColumnGap = `2rem`;
+    listContainer.style.gridTemplateColumns = `repeat(2, 1fr)`;
+  }
+  if (window.innerWidth >= 750) {
+    listContainer.style.gridTemplateRows = `repeat(${Math.round(listItems.length / 3)}, auto)`;
+    listContainer.style.gridTemplateColumns = `repeat(3, 1fr)`;
+  }
+  if (window.innerWidth >= 1024) {
+    listContainer.style.gridTemplateRows = `repeat(${Math.round(listItems.length / 4)}, auto)`;
+    listContainer.style.gridTemplateColumns = `repeat(4, 1fr)`;
+  }
+  if (window.innerWidth >= 2160) {
+    listContainer.style.gridTemplateRows = `repeat(${Math.round(listItems.length / 5)}, auto)`;
+    listContainer.style.gridTemplateColumns = `repeat(5, 1fr)`;
+  }
+};
