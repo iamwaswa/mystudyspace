01b350a9633b0b9728232c388cc5e40aa9e38ccb
waswaolunga <olungaw@gmail.com>
2019-03-22 01:08:50 -0700

    Complete simple design for edit and delete of comments

--- src/public/scripts/router.js
+++ src/public/scripts/router.js
@@ -64,12 +64,27 @@ router.post(`/studyspace/:_id/comments`, async (req, res) => {
     });
   }
 
-  res.redirect(`/studyspaces/${req.params._id}`);
+  res.redirect(`pages/studyspaces/${req.params._id}`);
+});
+
+router.get(`/studyspaces/:_id/comments/:comment_id/edit`, async (req, res) => {
+  const { text } = await Comment.findById(req.params.comment_id);
+
+  res.render(`pages/editcomment`, {
+    studyspaceId: req.params._id,
+    commentId: req.params.comment_id,
+    comment: text,
+  });
+});
+
+router.put(`/studyspaces/:_id/comments/:comment_id`, async (req, res) => {
+  await Comment.findByIdAndUpdate(req.params.comment_id, { text: comment });
+  res.redirect(`pages/studyspaces/${req.params._id}`);
 });
 
 router.delete(`/studyspaces/:_id/comments/:comment_id`, async (req, res) => {
   await Comment.findByIdAndDelete(req.params.comment_id);
-  res.redirect(`/studyspaces/${req.params._id}`);
+  res.redirect(`pages/studyspaces/${req.params._id}`);
 });
 
 router.get(`/*`, (req, res) => {
