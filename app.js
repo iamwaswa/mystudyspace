b50d09a11b7a473111319bd16b98940c958866e7
waswaolunga <olungaw@gmail.com>
2019-03-21 23:01:38 -0700

    Create router

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const StudySpace = require('./src/public/scripts/models');
-const getPlaceDetailsAsync = require('./src/public/scripts/create');
-const searchForPlaceAsync = require('./src/public/scripts/search');
+const router = require('./src/public/scripts/router');
 
 /**
  * Mongoose Setup
@@ -16,54 +14,10 @@ mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, autoIndex: fa
  */
 
 const app = express();
+app.set(`view engine`, `ejs`);
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.urlencoded({extended: true}));
-app.set(`view engine`, `ejs`);
-
-app.get(`/`, (req, res) => {
-  res.render(`pages/index`);
-});
-
-app.get(`/studyspaces`, async (req, res) => {
-  const studyspaces = await StudySpace.find({});
-  res.render(`pages/studyspaces`, { studyspaces });
-});
-
-app.get(`/studyspaces/new`, (req, res) => {
-  res.render(`pages/new`);
-});
-
-app.post(`/studyspaces/new`, async (req, res) => {
-
-  let studyspace;
-  if (req.body.placeId) {
-    studyspace = await getPlaceDetailsAsync(req.body.placeId);
-  } else if (req.body.place) {
-    studyspace = await searchForPlaceAsync(req.body.place);
-  }
-
-  const studyspaceExists = await StudySpace.findOne({ 
-    name: studyspace.name,
-    address: studyspace.address,
-    postalCode: studyspace.postalCode,
-  });
-
-  if (!studyspaceExists && studyspace) {
-    await StudySpace.create({ ...studyspace });
-  }
-  
-  res.redirect(`/studyspaces`);
-});
-
-app.get(`/studyspaces/:_id`, async (req, res) => {
-  const studyspace = await StudySpace.findById(req.params._id);
-  const studyspaces = await StudySpace.find({});
-  res.render(`pages/studyspace`, { studyspace, studyspaces });
-});
-
-app.get(`/*`, (req, res) => {
-  res.send(`The page you are looking for does not exist!`);
-});
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`MyStudySpace is running on port ${process.env.PORT}.
---
+++ src/public/scripts/router.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const StudySpace = require('./models/studyspace');
+const getPlaceDetailsAsync = require('./create');
+const searchForPlaceAsync = require('./search');
+
+router.get(`/`, (req, res) => {
+  res.render(`pages/index`);
+});
+
+router.get(`/studyspaces`, async (req, res) => {
+  const studyspaces = await StudySpace.find({});
+  res.render(`pages/studyspaces`, {
+    studyspaces
+  });
+});
+
+router.get(`/studyspaces/new`, (req, res) => {
+  res.render(`pages/new`);
+});
+
+router.post(`/studyspaces/new`, async (req, res) => {
+
+  let studyspace;
+  if (req.body.placeId) {
+    studyspace = await getPlaceDetailsAsync(req.body.placeId);
+  } else if (req.body.place) {
+    studyspace = await searchForPlaceAsync(req.body.place);
+  }
+
+  const studyspaceExists = await StudySpace.findOne({
+    name: studyspace.name,
+    address: studyspace.address,
+    postalCode: studyspace.postalCode,
+  });
+
+  if (!studyspaceExists && studyspace) {
+    await StudySpace.create({
+      ...studyspace
+    });
+  }
+
+  res.redirect(`/studyspaces`);
+});
+
+router.get(`/studyspaces/:_id`, async (req, res) => {
+  const studyspace = await StudySpace.findById(req.params._id);
+  const studyspaces = await StudySpace.find({});
+  res.render(`pages/studyspace`, {
+    studyspace,
+    studyspaces
+  });
+});
+
+router.get(`/*`, (req, res) => {
+  res.send(`The page you are looking for does not exist!`);
+});
+
+module.exports = router;
