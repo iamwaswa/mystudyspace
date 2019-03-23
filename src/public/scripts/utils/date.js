4408d2b9894af14ab1f16e3b44ac542f2b8b0c3c
waswaolunga <olungaw@gmail.com>
2019-03-23 01:07:08 -0700

    Setup for date implementation for comments

---
+++ src/public/scripts/utils/date.js
@@ -0,0 +1,19 @@
+const getCurrentDate = () => {
+  const date = new Date();
+};
+
+/* 
+A minute ago
+Half an hour ago
+An hour ago
+Yesterday
+On [day of the week]
+Last week
+Two weeks ago
+A month ago
+[number of months] ago
+A year ago
+[number of years] ago
+A decade ago
+[number of decades] ago
+*/
