3820b00dd9b82a081aed6b2dc22222a02efbd99f
waswaolunga <olungaw@gmail.com>
2019-04-03 20:11:07 -0700

    Finish designing menu

--- src/public/scripts/pages/drawermenu.js
+++ src/public/scripts/pages/drawermenu.js
@@ -3,12 +3,31 @@
 // =====================================
 const menuIcon = document.getElementById(`menu-icon`);
 const menu = document.getElementById(`menu`);
-
-window.onload = () => {
-  menu.style.display = `flex`;
-};
+const container = document.getElementsByClassName(`container`)[0];
 
 menuIcon.onclick = (event) => {
   event.preventDefault();
-  menu.classList.toggle(`show-menu`);
+  let toggled = false;
+  for (const className of menu.classList) {
+    if (className === `show-menu`) {
+      container ? container.classList.toggle(`seethrough`) : null;
+      menu.classList.add(`hide-menu`);
+      menu.classList.remove(`show-menu`);
+      toggled = true;
+      return;
+    }
+
+    if (className === `hide-menu`) {
+      container ? container.classList.toggle(`seethrough`) : null;      
+      menu.classList.add(`show-menu`);
+      menu.classList.remove(`hide-menu`);
+      toggled = true;
+      return;
+    }
+  }
+
+  if (!toggled) {
+    container ? container.classList.toggle(`seethrough`) : null;
+    menu.classList.add(`show-menu`);
+  }
 };
